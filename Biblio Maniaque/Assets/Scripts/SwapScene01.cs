aec0876488cb3df6634e79fe3bd2a7d6b0adbf07
DeschometsCedric <Ced.decho@gmail.com>
2023-05-04 07:44:16 +0100

    Unity Base

---
+++ Biblio Maniaque/Assets/Scripts/SwapScene01.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwapScene01 : MonoBehaviour
+{
+    public void OnClick(string sceneName)
+    {
+        SceneManager.LoadScene("pmmkkl");
+    }
+}
