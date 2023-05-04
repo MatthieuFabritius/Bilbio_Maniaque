a02ada3b80571114a8e2ae72851fe07d212c318b
Matthieu FABRITIUS <matthieu.fabritius@laposte.net>
2023-05-04 10:23:05 +0200

    menu scripts

--- Biblio Maniaque/Assets/Scripts/Pointer.cs
+++ Biblio Maniaque/Assets/Scripts/Pointer.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Pointer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public AudioSource audioSource;
+    public AudioClip PointerEnterSound;
+    public Button button;
+    
+
+
+    public void PointerEnter()
     {
-        
+        audioSource.PlayOneShot(PointerEnterSound);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  
+
 }
